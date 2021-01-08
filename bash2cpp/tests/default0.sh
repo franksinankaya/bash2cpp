b86b97b1fa710f7158d06ad1cf04350a692b12d0
Sinan Kaya <sinan.kaya@microsoft.com>
2021-01-07 21:59:44 -0500

    test

--- bash2cpp/tests/default0.sh
+++ bash2cpp/tests/default0.sh
@@ -1,2 +1,10 @@
 server=$1
+
+# If VAR is empty or unset, set the value of VAR to STRING. 
 echo ${server:=localhost} 
+
+default_value="def"
+other=1
+
+# If VAR is not empty, use STRING as its value. 
+echo ${other:+default_value}
